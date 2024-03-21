927bbb65dcc03931e1f2606a5c90f5a42368e410
RayRay <68286449+Ray70607@users.noreply.github.com>
2024-03-21 08:48:14 +0800

    Add files via upload

---
+++ run.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+saved_data = ""
+
+@app.route('/')
+def index():
+    return open("index.html", "r").read()
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    global saved_data
+    data = request.json['data']
+    saved_data = data
+    return "Data submitted successfully!"
+
+@app.route('/get_data')
+def get_data():
+    return saved_data
+
+if __name__ == '__main__':
+    app.run(debug=True)
